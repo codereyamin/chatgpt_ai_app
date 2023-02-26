2613d79ed96575ed72521951e7401f8b9ff7550a
Tuhin Ikbal Eyamin <88261555+codereyamin@users.noreply.github.com>
2023-02-26 21:30:40 +0600

    Update api_consts.dart

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
-String apiKey = "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";
+String apiKey = "sk-tE5daGllYhxRQr1MqzuET3BlbkfhdkhsdhkhFJXvZR3DbkSTg8JDp2u5wU";
 String baseUrl = "https://api.openai.com/v1";
